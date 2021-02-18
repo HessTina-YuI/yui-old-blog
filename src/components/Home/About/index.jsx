aaaab8f3c9d63dd0cb7622192c34d20ed8b1e29d
YuI <474766481@qq.com>
2021-02-19 00:25:53 +0800

    feature:修改 about 样式

--- src/components/Home/About/index.jsx
+++ src/components/Home/About/index.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
 import cls from 'classnames';
 import style from './index.module.less';
 
 class About extends Component {
     state = {
-        imgUrl: 'https://cdn.jsdelivr.net/gh/Trafalgar-YuI/img-bed@master/img/2021-02-18-01.png'
+        imgUrl: 'https://cdn.jsdelivr.net/gh/Trafalgar-YuI/img-bed@master/img/2020-02-19-01.png'
     };
 
     render() {
@@ -15,19 +14,16 @@ class About extends Component {
         return (
             <section className={style.section}>
                 <div className={context}>
-                    <Row className={style.aboutTitle}>
-                        <Col span={24}>
-                            <div className={style.aboutTitleText}>
-                                About Me
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row className={style.aboutContext}>
-                        <Col span={8} style={{background: `url(${imgUrl})`}}>
-                        </Col>
-                        <Col span={16} style={{background: "yellow"}}>
-                        </Col>
-                    </Row>
+                    <div className={style.aboutTitle}>
+                        <div className={style.aboutTitleText}>
+                            About Me
+                        </div>
+                    </div>
+                    <div className={style.aboutContext}>
+                        <div className={style.aboutContextLeft} style={{backgroundImage: `url(${imgUrl})`}}/>
+                        <div className={style.aboutContextRight} >
+                        </div>
+                    </div>
                 </div>
             </section>
         );
